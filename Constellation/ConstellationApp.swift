9a0829b606be56425da1cd34bb533789caec9567
Raghav Mangrola <dev@raghav.io>
2025-06-02 23:24:16 -0700

    Initial Commit

---
+++ Constellation/ConstellationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConstellationApp.swift
+//  Constellation
+//
+//  Created by Raghav Mangrola on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ConstellationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
